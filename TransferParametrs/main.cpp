c6b16750bbf90172f100a64137f68520a41261ea
Игорь Петров <igor567-1986@mail.ru>
2022-08-30 22:21:03 +0400

    ClassWork end

--- TransferParametrs/main.cpp
+++ TransferParametrs/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void Exchange(int a, int b);
+void Exchange(int& a, int& b);
 
 void main()
 {
@@ -14,7 +14,7 @@ void main()
 
 }
 
-void Exchange(int a, int b)
+void Exchange(int& a, int& b)
 {
 	int buffer = a;
 	a = b;
