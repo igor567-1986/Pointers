28667e1e7fa31802fa592e3d83b7a320a0b1a9f7
Игорь Петров <igor567-1986@mail.ru>
2022-09-12 20:46:17 +0400

    homeWorkDay2

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -10,6 +10,8 @@ int* push_back(int* arr, int& n, int valye);
 int* push_front(int* arr, int& n, int valye);
 int* insert(int* arr, int& n, int valye,int index);
 int* pop_back(int* arr, int& n);
+int* pop_front(int* arr, int& n);
+int* erase(int* arr, int& n,int index);
 
 
 void main()
@@ -20,8 +22,8 @@ void main()
 	int* arr = new int[n];
 	FillRand(arr, n);
 	Print(arr, n);
+	int valye, index;
 #ifdef part_1
-    int valye,index;
 	cout << "¬ведите добавл€емое число в конец массива:"; cin >> valye;
 	arr = push_back(arr, n, valye);
 	Print(arr, n);
@@ -38,10 +40,20 @@ void main()
 	arr = insert(arr, n, valye, index);
 	Print(arr, n);
 #endif // 
-	pop_back( arr, n);
+	arr = pop_back( arr, n);
+	Print(arr, n);
+	arr = pop_front(arr, n);
 	Print(arr, n);
 	delete[]arr;
-
+	cout << "¬ведите индекс по каторому надо удалить число в массиве он не должен быть выше" << n - 1 << ":"; cin >> index;
+	do
+		if (index > n - 1)
+		{
+			cout << "ќшибка индекс введен не верно ,введите его заново"; cin >> index;
+		}
+	while (index > n - 1);
+	arr = erase(arr, n,index);
+	Print(arr, n);
 }
 
 void FillRand(int arr[], const int n)
@@ -120,6 +132,32 @@ int* pop_back(int* arr, int& n)
 	for (int i = 0; i < n-1; i++)
 	{
 		buffer[i] = arr[i];
+	}
+	delete[]arr;
+	arr = buffer;
+	n--;
+	return arr;
+}
+int* pop_front(int* arr, int& n)
+{
+	int* buffer = new int[n - 1];
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		buffer[i] = arr[i+1];
+	}
+	delete[]arr;
+	arr = buffer;
+	n--;
+	return arr;
+}
+int* erase(int* arr, int& n,int index)
+{
+	int* buffer = new int[n - 1];
+	for (int i = 0; i < n-1 ; i++)
+	{
+		if (i < index) buffer[i] = arr[i];
+		else buffer[i] = arr[i+1];
 	}
 	delete[]arr;
 	arr = buffer;
